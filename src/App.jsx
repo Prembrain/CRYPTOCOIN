ec06ec495101708bcf9016b47d540ec9d1037d1c
Prembrain <140951542+Prembrain@users.noreply.github.com>
2024-06-22 21:34:23 +0700

    Update App.jsx
    refresh page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 import {Navbar, Home, Feature, Howto, Trade, Learn, Footer} from "./components"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden scroll-smooth">
       <Navbar />
-      <Routes>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/feature" element={<Feature />} />
-        <Route path="/howto"  element={<Howto />}/> */}
         <Route path="/trade"  element={<Trade />}/>
         <Route path="/learn"  element={<Learn />}/>
       </Routes>
@@ -20,4 +20,3 @@ export default function App() {
     </>
   );
 }
-
